88aae8c9e9be28ca0fbbe9b35860da968949547e
su-san1129 <deviationvalue1129@icloud.com>
2019-10-24 13:10:06 +0900

    f

---
+++ src/Car.java
@@ -0,0 +1,28 @@
+
+public class Car {
+	
+	int speed;
+	int name;
+	int gas;
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+	public int getGas() {
+		return gas;
+	}
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	
+
+}
